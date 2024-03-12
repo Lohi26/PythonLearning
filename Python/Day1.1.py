967c07f2d51ee7343cf0e1a378faa71239b7c4ae
Lohi26 <lohitachandru@gmail.com>
2024-03-12 10:30:04 +0530

    day 1 and 2 of Python

---
+++ Python/Day1.1.py
@@ -0,0 +1,18 @@
+print("Hello")
+
+
+print('hello! i am "lohita" ')   #use single quote to print the double quote or use back shales
+
+print("hello i am \"fine\" how are you?")
+
+print("lohita\nlohita\nlohita")
+
+#    print("Lohita" + " Chandru")   #indendation error occurs when we use a extra space 
+
+print("My name is = "+input("Enter your name="))
+
+n=input("Enter your age=")
+print("My age is="+n)
+
+print(len(input()))  #len to find the lenght of the string
+
---
+++ Python/Day1.2.py
@@ -0,0 +1,13 @@
+name="Lohita!"
+print(name)
+name="Minnie"
+print(name)
+
+a=input()
+b=input()
+print(a,b)
+temp=a
+a=b
+b=temp
+print(a,b)
+
