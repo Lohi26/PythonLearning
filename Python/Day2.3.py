a95a92d02b6221421d62cb163726069e472d7e60
Lohi26 <lohitachandru@gmail.com>
2024-03-12 11:52:15 +0530

    day 2 fully

---
+++ Python/Day2.3.py
@@ -0,0 +1,21 @@
+print(8/3)
+
+print(round(8/3))
+
+print(round(2.411))
+
+print(round(2.511))
+
+
+print(round(2.819,0))
+print(round(2.819,1))
+print(round(2.819,2))
+
+print(8 // 3)
+
+
+name="Lohita"
+age=21
+native="India"
+
+print(f"My name is {name}, a {age} years old teenager from {native}")
---
+++ Python/Day2.4.py
@@ -0,0 +1,6 @@
+#Calculate the number of weeks a human going to live by using their entered age
+
+age=int(input("Enter your age="))
+year=100-age
+weeks=year*52
+print(f"Your lifetime's week number was {weeks}")
