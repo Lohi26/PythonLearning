cc0759e3440db96bffdc18991c01ffa11d23dead
Lohi26 <lohitachandru@gmail.com>
2024-03-12 12:04:38 +0530

    Day 3

---
+++ Python/Day3.1.py
@@ -0,0 +1,14 @@
+number=int(input())
+if number>10:
+    print("YES")
+else:
+    print("NO")
+
+
+height=int(input("Enter the height(in cms) = "))
+if height>120:
+    print("Can ride")
+    print("🥳Hurray!")
+else:
+    print("Can't ride")
+    print("🥲Opps!")
---
+++ Python/Day3.2.py
@@ -0,0 +1,5 @@
+number=int(input("Enter the number to be checked = "))
+if number%2==0:
+    print(f"{number} is a even number")
+else:
+    print(f"{number} is a odd number")
