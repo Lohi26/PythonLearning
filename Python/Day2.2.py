d1b3e8eba3b30d7534c6f400976b0496e31699b3
Lohi26 <lohitachandru@gmail.com>
2024-03-12 11:02:44 +0530

    Day 2 and continue

---
+++ Python/Day2.2.py
@@ -0,0 +1,20 @@
+# + - * / **
+
+# ()
+# **
+# *
+# 
+# /
+# +
+# -
+
+print(3*3+3/3-3)
+
+print(3*(3+3)/(3*3))
+
+
+#BMI
+weight=int(input("Enter your weight="))
+height=float(input("Enter your height="))
+BMI=weight/((height/100)**2)
+print(BMI)
