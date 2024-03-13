69000fc21a1a1eeaa1188e1c2add73ef15049e42
Lohi26 <lohitachandru@gmail.com>
2024-03-13 09:21:43 +0530

    renames

---
+++ Python/Day4/Day41.py
@@ -0,0 +1,8 @@
+import random
+import Day
+print("Lohita"[0])
+a=random.randint(1,20)
+print(a)
+#random module is a python module
+#large code is splited into individual modules 
+#each module does it's task
