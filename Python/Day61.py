977fae45a4e5e58aea3e5f2d19917a8789ada37d
Lohi26 <lohitachandru@gmail.com>
2024-04-01 17:27:11 +0530

    Day 6

---
+++ Python/Day61.py
@@ -0,0 +1,33 @@
+#functions
+
+
+arr=[1,False,3,4]
+print(all(arr))
+
+
+arr1=[1,1,2,2,4,5,6]
+print(all(set(arr)))
+
+print(sum(arr1))
+
+
+
+## Writing the own function 
+
+
+
+def printing():
+    print("Hai, I am Lohita Chandrasekaran!!")
+    print("I am doing well, How are you?")
+
+printing()
+
+
+a=int(input())
+b=int(input())
+def add(x,y):
+    return x+y
+print(add(a,b))
+
+
+
