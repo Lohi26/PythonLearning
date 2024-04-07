6e3db72afa28204fa249e7fa549eb85845baaca3
Lohi26 <lohitachandru@gmail.com>
2024-04-07 09:02:57 +0530

    Day 7 Hangman Game

--- Python/HangmanGame/hangman.py
+++ Python/HangmanGame/hangman.py
@@ -1,13 +1,21 @@
 #Step 1 
 import random
-word_list = ["Peacock", "Baboon", "Camel","Dogs","Triger"]
-
+import hangman_words 
 #TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
 
 #TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
 
 #TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
-
+logo = r''' 
+ _                                             
+| |                                            
+| |__   __ _ _ __   __ _ _ __ ___   __ _ _ __  
+| '_ \ / _` | '_ \ / _` | '_ ` _ \ / _` | '_ \ 
+| | | | (_| | | | | (_| | | | | | | (_| | | | |
+|_| |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|
+                    __/ |                      
+                   |___/    '''
+print(logo)
 stages = [r'''
   +---+
   |   |
@@ -64,9 +72,8 @@ stages = [r'''
       |
 =========
 ''']
-
-index=random.randint(0,len(word_list)-1)
-word=word_list[index].lower()
+index=random.randint(0,len(hangman_words.word_list)-1)
+word=hangman_words.word_list[index].lower()
 
 blank=["_"]*len(word)
 boolean=True
@@ -75,6 +82,8 @@ ind=len(stages)-1
 
 while boolean:
     user=input("Guess a letter....!!\n")
+    if user in word:
+        print("You have already guesses the letter...Please try different...!!!")
     letter_list=list(word)
     for i in range(len(letter_list)):
         if user==letter_list[i]:
