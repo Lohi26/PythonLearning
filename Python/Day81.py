7c1f05dbf9db32cbbe0480370e844d055907c088
Lohi26 <lohitachandru@gmail.com>
2024-04-07 10:04:18 +0530

    Function with parameters

---
+++ Python/Day81.py
@@ -0,0 +1,16 @@
+#Functions with arguments..
+
+def greed():
+    print("Hello everyone!")
+    print("I am Python")
+    print("Would you like to learn me?")
+
+greed()
+
+
+
+def add(x,y):
+    return x+y
+
+
+print(add(10,20))
